dfd694689d0c9d1d63c645ec926523e217e5eafa
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2022-08-27 13:59:58 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 const Footer = () => {
 
         return(
-                <>
-                </>
+                <div className="footer">
+                        <div className="footer-inner">
+                                <div className="footer-name m-l-1">
+                                        © 2022 Церковь Слово Жизни г. Столин
+                                </div>
+                                <div className="footer-social">
+                                        <div>C</div>
+                                        <div>V</div>
+                                </div>
+                        </div>
+                </div>
         )
 }
 

af3e3b9813d71b7585a2e4f666db74eabf333d92
unknown <C:\Users\Victor\AppData\Roaming\The Bat! Pwd>
2022-09-03 16:15:11 +0300

    add contacts

--- src/components/main/Contact.jsx
+++ src/components/main/Contact.jsx
@@ -1,4 +1,28 @@
-import { Title } from 'components'
+import { Title, ButtonMore } from 'components'
+import placeholder from 'assets/svg/placeholder.svg'
+import phone from 'assets/svg/phone.svg'
+import email from 'assets/svg/email.svg'
+import clock from 'assets/svg/clock.svg'
+
+const Contacts = [
+        {
+            text: 'г. Столин, \nул. Советская 94Б', 
+            logo: placeholder,
+        },
+        {
+            text: '+375(29) 201 62 38', 
+            logo: phone,
+        },
+        {
+            text: 'alsheuskivictor@gmail.com', 
+            logo: email,
+        },
+
+        {
+            text: 'Собрания: \nкаждое воскресени в 10:00 и 17:00', 
+            logo: clock,
+        }
+]
 
 const Contact = () => {
         return(
@@ -9,6 +33,26 @@ const Contact = () => {
                                                 name={'contact'}
                                                 darkMode={false}
                                         />
+                                        <div className="contact-block contact-items-grid">
+                                            {
+                                                    Contacts.map(({text, logo}, i)=> (
+                                                            <div className={`contact-item c-p `} key={text}>
+                                                                <div>
+                                                                        <img src={logo} alt="logos" className="contact-item-logo" />
+                                                                </div>
+                                                                <div> {text}</div>
+                                                               
+                                                            </div>
+                                                    ))
+                                            }
+                                    </div>
+
+                                    <div className="button-more-wrapper">
+                                        <ButtonMore
+                                                className='button-n-hover'
+                                                title={'Подробные контакты'}
+                                        />
+                                </div>
                                 </div>
                         </div>
                 </div>

6cd04346a957678af1895c7665944973d517a701
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2022-09-03 14:29:46 +0300

    acordeon 0.2 version

--- src/components/main/modules/Accordion.jsx
+++ src/components/main/modules/Accordion.jsx
@@ -1,17 +1,17 @@
 import {questions} from './ArrayBlocks'
 
 const Accordion = () => {
-        
+
 
         return(
-                <div className='questions-block-accordion w-100 m-t-4'>
+                <div className='questions-block-accordion w-100 m-t-4 m-l-4'>
                         {
                                 questions.map(({title, description}, i) => (
-                                        <>
-                                                <input id={`toggle${i}`} type="checkbox" name="toggle" key={i}/>
-                                                <label for={`toggle${i}`} key={i}>{title}</label>
-                                                <section id={`content${i}`} key={i}><p  className='color-w-g'>{description}</p></section>
-                                        </>
+                                        <div>
+                                                <input id={`toggle${i}`} type="checkbox" name="toggle" key={i}></input>
+                                                <label for={`toggle${i}`} key={i} id={`label${i}`}>{title}</label>
+                                                <section id={`content${i}`} key={i}>{description}</section>
+                                        </div>
                                 ))
                         }
                 </div>

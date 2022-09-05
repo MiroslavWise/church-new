6764fe8925974efc18fb82160f13d99d8a2bff13
unknown <C:\Users\Victor\AppData\Roaming\The Bat! Pwd>
2022-09-05 23:45:56 +0300

    contact animation

--- src/components/main/Contact.jsx
+++ src/components/main/Contact.jsx
@@ -1,8 +1,16 @@
 import { Title, ButtonMore } from 'components'
 import { contacts } from './modules/ArrayBlocks'
-import {ScrollInfoBottom} from 'assets/func'
+import AOS from 'aos'
 
 const Contact = () => {
+
+        AOS.init({
+                offset: 60,
+                delay: 200,
+                duration: 300,
+                easing: 'ease-in-out',
+        })
+
         return(
                 <div className="w-100 contact" id='main-contact'>
                         <div className="block">
@@ -14,7 +22,7 @@ const Contact = () => {
                                         <div className="contact-items contact-items-grid">
                                                 {
                                                         contacts.map(({text, logo, link})=> (
-                                                                <div className={`contact-item c-p `} key={text} >
+                                                                <div className={`contact-item c-p`} data-aos="zoom-in" key={text} >
                                                                         <div>
                                                                                 <img src={logo} alt="logos" className="contact-item-logo" />
                                                                         </div>
